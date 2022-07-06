162064f3d596eafc3bb55212ca260e3aca30b4f0
Martin3stte <108824470+Martin3stte@users.noreply.github.com>
2022-07-06 18:55:40 +0200

    Add files via upload

---
+++ martin3stte_calculator.py
@@ -0,0 +1,37 @@
+
+# Calculator by Martín Estévez López. All Credits Reserved.
+
+def sum(a, b):
+    return (a + b)
+
+a = int(input('Sum. Enter 1st number: '))
+b = int(input('Enter 2nd number: '))
+
+print(f'Sum of {a} and {b} is {sum(a, b)}')
+
+def sub(c, d):
+    return (c - d)
+
+c = int(input('Substraction. Enter 1st number: '))
+d = int(input('Enter 2nd number: '))
+
+print(f'sub of {c} and {d} is {sub(c, d)}')
+
+def multiplication(e, f):
+    return (e * f)
+
+e = int(input('Multiplication. Enter 1st number: '))
+f = int(input('Enter 2nd number: '))
+
+print(f'Multiplication of {e} and {f} is {multiplication(e, f)}')
+
+def div(g, h):
+    return (g / h)
+
+g = int(input('Division. Enter 1st number: '))
+h = int(input('Enter 2nd number: '))
+
+print(f'Division of {g} and {h} is {div(g, h)}')
+
+
+print("Thanks for using Martín Estévez's calculator.")
